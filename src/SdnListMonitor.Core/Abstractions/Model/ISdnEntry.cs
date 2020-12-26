7ce9529678babff37d573b0a0de83ca3371a8d49
Tomas <tomas.stuina@gmail.com>
2020-12-26 16:13:43 +0200

    Add ISdnDataProvider and related interfaces for describing the retrieval of SDN entries

---
+++ src/SdnListMonitor.Core/Abstractions/Model/ISdnEntry.cs
@@ -0,0 +1,20 @@
+﻿namespace SdnListMonitor.Core.Abstractions.Model
+{
+    /// <summary>
+    /// Represents an entry of Specially Designated Nationals List.
+    /// </summary>
+    public interface ISdnEntry
+    {
+        public int Uid { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+
+        public string Title { get; }
+
+        public string SdnType { get; }
+
+        public string Remarks { get; }
+    }
+}
