378f1c6cf253f07c241767aa7ce8bf1451b5158c
tomas.stuina <tomas.stuina@gmail.com>
2020-12-26 16:18:23 +0200

    Add SdnXmlDataProvider  that provides the retrieval of the Specially Designated Nationals List entries from SDN.xml (#2)
    * Add ISdnDataProvider and related interfaces for describing the retrieval of SDN entries
    
    * Add SdnXmlDataProvider that provides the retrieval of the Specially Designated Nationals List entries from SDN.xml file that corresponds to the format defined in SDN.xsd schema file  (https://home.treasury.gov/system/files/126/sdn.xsd)

---
+++ src/SdnListMonitor.Core/Service/Xml/SdnXmlDataProvider.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Options;
+using SdnListMonitor.Core.Abstractions.Model;
+using SdnListMonitor.Core.Abstractions.Service;
+using SdnListMonitor.Core.Abstractions.Service.Xml;
+using SdnListMonitor.Core.Configuration.Xml;
+using SdnListMonitor.Core.Model.Xml;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SdnListMonitor.Core.Service.Xml
+{
+    /// <summary>
+    /// Provides the retrieval of the Specially Designated Nationals List entries
+    /// from SDN.xml file that corresponds to the format defined in SDN.xsd schema file
+    /// (https://home.treasury.gov/system/files/126/sdn.xsd).
+    /// </summary>
+    public class SdnXmlDataProvider : ISdnDataProvider
+    {
+        private readonly IXmlReaderFactory m_xmlReaderFactory;
+        private readonly SdnXmlDataProviderOptions m_options;
+        private readonly XmlSerializer m_xmlSerializer;
+        private readonly XmlReaderSettings m_xmlReaderSettings;
+
+        private const string SdnXmlDefaultNamespace = "http://tempuri.org/sdnList.xsd";
+        private const string SdnListNodeName = "sdnList";
+
+        public SdnXmlDataProvider (IXmlReaderFactory xmlReaderFactory, IOptions<SdnXmlDataProviderOptions> options)
+        {
+            m_xmlReaderFactory = xmlReaderFactory ?? throw new ArgumentNullException (nameof (xmlReaderFactory));
+            m_options = options?.Value ?? throw new ArgumentNullException (nameof (options));
+            m_xmlSerializer = new XmlSerializer (typeof (SdnXmlEntry), SdnXmlDefaultNamespace);
+            m_xmlReaderSettings = new XmlReaderSettings { Async = true, IgnoreWhitespace = true, IgnoreComments = true };
+        }
+
+        /// <summary>
+        /// Gets all the Specially Designated Nationals List entries from the SDN.xml, located in the configured path.
+        /// </summary>
+        /// <remarks>
+        /// The SDN.xml file is read by streaming it fragment by fragment (<sdnEntry/> node). This allows to process
+        /// SDN List entries as we go, instead of loading the whole SDN.xml file to the memory and then processing it.
+        /// Though, this also depends how much the data is being buffered by the underlying stream in XML reader.
+        /// </remarks>
+        /// <param name="cancellationToken">The cancellation token to cancel operation.</param>
+        /// <returns>An <see cref="IAsyncEnumerable{ISdnEntry}"/> that contains all the entries.</returns>
+        public async IAsyncEnumerable<ISdnEntry> GetSdnEntriesAsync (CancellationToken cancellationToken = default)
+        {
+            using XmlReader xmlReader = m_xmlReaderFactory.Create (m_options.XmlFilePath, m_xmlReaderSettings);
+
+            ReadToRootNode (xmlReader);
+
+            await foreach (var sdnEntry in ReadSdnEntriesAsync (xmlReader))
+                yield return sdnEntry;
+        }
+
+        private void ReadToRootNode (XmlReader xmlReader)
+        {
+            try
+            {
+            // Consider SDN.xml file malformed if <sdnList/> root node is not present.
+            if (!xmlReader.ReadToDescendant (SdnListNodeName))
+                throw new InvalidOperationException (Res.ErrorWhileRetrievingSdnList);
+            }
+            catch (XmlException e)
+            {
+                throw new InvalidOperationException (Res.ErrorWhileRetrievingSdnList, e);
+            }
+        }
+
+        private async IAsyncEnumerable<ISdnEntry> ReadSdnEntriesAsync (XmlReader xmlReader)
+        {
+            while (await xmlReader.ReadAsync ().ConfigureAwait (false))
+            {
+                // Skip all the nodes that are not <sdnEntry/> as we are only interested in those.
+                if (!xmlReader.IsStartElement () || xmlReader.Name != SdnXmlEntry.SdnEntryNodeName)
+                    continue;
+
+                // Consider SDN.xml file malformed if we are unable to deserialize one of the <sdnList/> nodes.
+                if (!m_xmlSerializer.CanDeserialize (xmlReader))
+                    throw new InvalidOperationException (Res.ErrorWhileRetrievingSdnList);
+
+
+                var sdnEntry = m_xmlSerializer.Deserialize (xmlReader) as ISdnEntry;
+                yield return sdnEntry;
+            }
+        }
+    }
+}
---
+++ tests/SdnListMonitor.Core.Tests/Service/Xml/SdnXmlDataProviderTests.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using SdnListMonitor.Core.Abstractions.Service.Xml;
+using SdnListMonitor.Core.Configuration.Xml;
+using SdnListMonitor.Core.Model.Xml;
+using SdnListMonitor.Core.Service.Xml;
+using Shouldly;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+
+namespace SdnListMonitor.Core.Tests.Service
+{
+    public class SdnXmlDataProviderTests
+    {
+        private readonly IOptions<SdnXmlDataProviderOptions> m_options;
+
+        public SdnXmlDataProviderTests ()
+        {
+            m_options = Options.Create (new SdnXmlDataProviderOptions ());
+        }
+
+        [Fact]
+        public void Ctor_WhenXmlReaderFactoryNull_ShouldThrowArgumentNullException ()
+        {
+            // Act & Assert
+            Should.Throw<ArgumentNullException> (() => new SdnXmlDataProvider (null, m_options))
+                  .ParamName
+                  .ShouldBe ("xmlReaderFactory");
+        }
+
+        [Fact]
+        public void Ctor_WhenOptionsNull_ShouldThrowArgumentNullException ()
+        {
+            // Act & Assert
+            Should.Throw<ArgumentNullException> (() => new SdnXmlDataProvider (Mock.Of<IXmlReaderFactory> (), null))
+                  .ParamName
+                  .ShouldBe ("options");
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenFilePathIsSetInOptions_ShouldCreateXmlReaderWithPresetPath ()
+        {
+            // Arrange
+            m_options.Value.XmlFilePath = Guid.NewGuid ().ToString ();
+            var xmlReader = new Mock<XmlReader> ();
+            xmlReader.Setup (self => self.ReadToDescendant (It.IsAny<string> ())).Returns (true);
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader.Object);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ();
+
+            // Assert
+            Mock.Get (xmlReaderFactory).Verify (self => self.Create (
+                m_options.Value.XmlFilePath, 
+                It.IsAny<XmlReaderSettings> ()), 
+            Times.Once);
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenXmlReaderIsReadyToBeCreated_ShouldSetXmlReaderSettings()
+        {
+            // Arrange
+            var xmlReader = new Mock<XmlReader> ();
+            xmlReader.Setup (self => self.ReadToDescendant (It.IsAny<string> ())).Returns (true);
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader.Object);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ();
+
+            // Assert
+            Mock.Get (xmlReaderFactory).Verify (self => self.Create (
+                It.IsAny<string> (), 
+                It.Is<XmlReaderSettings> (settings =>
+                    settings.Async && settings.IgnoreComments && settings.IgnoreWhitespace)), 
+            Times.Once);
+        }
+
+        [Fact]
+        public void GetSdnEntriesAsync_WhenSdnXmlDoesNotContainSdnList_ShouldThrowInvalidOperationException ()
+        {
+            // Arrange
+            var stream = CreateStreamFromString ("");
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act & Assert
+            var exception = Should.Throw<InvalidOperationException> (async () => await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ());
+            exception.Message.ShouldBe ("An error occurred while retrieving SDN List.");
+            exception.InnerException.ShouldBeOfType<XmlException> ();
+        }
+
+        [Fact]
+        public void GetSdnEntriesAsync_WhenSdnXmlContainsUnknownRootElement_ShouldThrowInvalidOperationException ()
+        {
+            // Arrange
+            var sdnListXml = new XElement ("UnknownElement");
+            var stream = CreateStreamFromString (sdnListXml.ToString ());
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            Should.Throw<InvalidOperationException> (async () => await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ())
+                  .Message
+                  .ShouldBe ("An error occurred while retrieving SDN List.");
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenSdnListDoesNotContainSdnEntries_ShouldReturnEmptyEnumeration ()
+        {
+            // Arrange
+            var sdnListXml = CreateSdnListXElement ();
+            var stream = CreateStreamFromString (sdnListXml.ToString ());
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            var sdnEntries = await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ();
+
+            // Assert
+            sdnEntries.ShouldBeEmpty ();
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenSdnListContainsSdnEntries_ShouldReturnSdnEntries ()
+        {
+            // Arrange
+            var expectedSdnEntries = new[] { new SdnXmlEntry { Uid = 1 }, new SdnXmlEntry { Uid = 2 }, new SdnXmlEntry { Uid = 3 } };
+            var sdnListXml = CreateSdnListXElement (expectedSdnEntries);
+            var str = sdnListXml.ToString ();
+            var stream = CreateStreamFromString (str);
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            var actualSdnEntries = await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ();
+
+            // Assert
+            actualSdnEntries.Length.ShouldBe (expectedSdnEntries.Length);
+            for (int i = 0; i < expectedSdnEntries.Length; i++)
+                actualSdnEntries[i].Uid.ShouldBe (expectedSdnEntries[i].Uid);
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenSdnEntryIsDeserialized_ShouldReturnSdnEntryWithExactValues ()
+        {
+            // Arrange
+            var sdnXmlEntry = new SdnXmlEntry () 
+            { 
+                Uid = 1,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title",
+                SdnType = "SdnType",
+                Remarks = "Remarks"
+            };
+            var sdnListXml = CreateSdnListXElement (sdnXmlEntry);
+            var stream = CreateStreamFromString (sdnListXml.ToString ());
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            var sdnEntry = await sdnXmlDataProvider.GetSdnEntriesAsync ().SingleAsync ();
+
+            // Assert
+            sdnEntry.ShouldSatisfyAllConditions (
+                () => sdnEntry.Uid.ShouldBe (sdnXmlEntry.Uid),
+                () => sdnEntry.FirstName.ShouldBe (sdnXmlEntry.FirstName),
+                () => sdnEntry.LastName.ShouldBe (sdnXmlEntry.LastName),
+                () => sdnEntry.Title.ShouldBe (sdnXmlEntry.Title),
+                () => sdnEntry.SdnType.ShouldBe (sdnXmlEntry.SdnType),
+                () => sdnEntry.Remarks.ShouldBe (sdnXmlEntry.Remarks)
+                );
+        }
+
+        [Fact]
+        public void GetSdnEntriesAsync_WhenSdnListContainMalformedSdnEntry_ShouldThrowInvalidOperationException ()
+        {
+            // Arrange
+            var sdnListXml = new XElement ("sdnList", new XElement ("sdnEntry", "Malformed content"));
+            var stream = CreateStreamFromString (sdnListXml.ToString ());
+            var xmlReader = XmlReader.Create (stream, new XmlReaderSettings { Async = true });
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act & Assert
+            Should.Throw<InvalidOperationException> (async () => await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ())
+                  .Message
+                  .ShouldBe ("An error occurred while retrieving SDN List.");
+        }
+
+        [Fact]
+        public async Task GetSdnEntriesAsync_WhenSdnEntriesAreReturned_ShouldDisposeXmlReader ()
+        {
+            // Arrange
+            var xmlReader = new Mock<XmlReader> ();
+            xmlReader.Setup (self => self.ReadToDescendant (It.IsAny<string> ())).Returns (true);
+            var disposableXmlReader = xmlReader.As<IDisposable> ();
+            var xmlReaderFactory = CreateXmlReaderFactory (xmlReader.Object);
+            var sdnXmlDataProvider = new SdnXmlDataProvider (xmlReaderFactory, m_options);
+
+            // Act
+            await sdnXmlDataProvider.GetSdnEntriesAsync ().ToArrayAsync ();
+
+            // Assert
+            disposableXmlReader.Verify (self => self.Dispose (), Times.Once);
+        }
+
+        private static Stream CreateStreamFromString (string text) =>
+            new MemoryStream (Encoding.UTF8.GetBytes (text));
+
+        private static XElement CreateSdnListXElement (params SdnXmlEntry[] sdnXmlEntries)
+        {
+            XNamespace ns = "http://tempuri.org/sdnList.xsd";
+            var sdnEntryXElements = sdnXmlEntries.Select (entry => CreateXElementFrom (ns, entry)).ToArray ();
+            return new XElement (ns + "sdnList", sdnEntryXElements);
+        }
+
+        private static XElement CreateXElementFrom (XNamespace ns, SdnXmlEntry sdnXmlEntry)
+        {
+            return new XElement (ns + "sdnEntry",
+                new XElement (ns + "uid", sdnXmlEntry.Uid),
+                new XElement (ns + "firstName", sdnXmlEntry.FirstName),
+                new XElement (ns + "lastName", sdnXmlEntry.LastName),
+                new XElement (ns + "title", sdnXmlEntry.Title),
+                new XElement (ns + "sdnType", sdnXmlEntry.SdnType),
+                new XElement (ns + "remarks", sdnXmlEntry.Remarks)
+                );
+        }
+
+        private static IXmlReaderFactory CreateXmlReaderFactory (XmlReader xmlReader) =>
+            Mock.Of<IXmlReaderFactory> (self => self.Create (It.IsAny<string> (), It.IsAny<XmlReaderSettings> ()) == xmlReader);
+    }
+}
